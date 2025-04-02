1b32d59d42f9207deabf827163db4518fba2bedb
Nastya Lit <nastenkakurochk@gmail.com>
2025-04-02 08:44:51 +0300

    Первая попытка

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,28 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template');
 
 // @todo: DOM узлы
+const cardsList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCards(dataArray, cardTmp, container) {
+    dataArray.forEach(element => {
+      const cardElement = cardTmp.content.cloneNode(true);
+      const cardTitle = cardElement.querySelector('.card__title');
+      const cardImage = cardElement.querySelector('.card__image');
+      const deleteButton = cardElement.querySelector('.card__delete-button');
+      cardTitle.textContent = element.name;
+      cardImage.src = element.link;
+      deleteButton.addEventListener('click', deleteCard);
+      container.append(cardElement); 
+    });
+}
 
 // @todo: Функция удаления карточки
+function deleteCard (event) {
+    const eventTarget = event.target;
+    eventTarget.closest('.card').remove(); 
+}
 
 // @todo: Вывести карточки на страницу
+createCards(initialCards, cardTemplate, cardsList);
